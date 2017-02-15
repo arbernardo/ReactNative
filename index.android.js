ad7cd6a1836f60d9993d96d364feee9002e7c30f
ace <arbernardo23@gmail.com>
2017-02-16 01:27:16 +0800

    adding another barchart

--- index.android.js
+++ index.android.js
@@ -13,13 +13,18 @@ import {
   Dimensions,
 } from 'react-native';
 
+import * as d3 from 'd3';
+
 import BarChart from './js/BarChart';
 import PieChart from './js/PieChart';
+import BarChart2 from './js/BarChart2';
+import tsv from './resources/data.tsv';
 
 export default class Testing extends Component {
   render() {
 
     var data1 = [21, 43, 32, 7, 11, 14, 31, 11, 14, 31, 33];
+    var data2 = [44, 10, 51, 16, 24];
     var piedata = [
       {"number":  8, "name": 'Fun activities'},
       {"number": 7, "name": 'Dog'},
@@ -29,10 +34,19 @@ export default class Testing extends Component {
       {"number":  4, "name": 'Misc'},
     ];
 
+    var bar2Data;
+
+    // d3.tsv(file, function(d){return d.frequency;}, function(error, data){
+    //   console.log(data);
+    // });
+
+    // console.log(file);
+
     return (
       <View style={styles.container}>
         <BarChart data={data1}/>
-        <PieChart data={piedata}/>
+        {/*<PieChart data={piedata}/>*/}
+        <BarChart2 width={320} height={240} data={data2}/>
       </View>
     );
   }
