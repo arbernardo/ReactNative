37e89a75678e492096d8bc0610feef4d0e1e6f4b
ace <arbernardo23@gmail.com>
2017-02-17 02:03:14 +0800

    moved directories, added MainApp.js and FeatureList.js

---
+++ js/FeatureList.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Ace on 2/17/2017.
+ */
---
+++ js/MainApp.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Ace on 2/17/2017.
+ */
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class MainApp extends Component {
+
+
+    render() {
+
+
+        return (
+            <View>
+            </View>
+        );
+    }
+}
