0c83e11c5ca749ca7354fb01a52af7e1e81467cb
abernardo <abernardo@wfgmb.com>
2017-02-20 19:53:45 +0800

    Add color theme

---
+++ js/charts/theme/Color.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ace on 2/20/17.
+ */
+export default {
+
+    colors: [
+        "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+        "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
+    ]
+
+}
